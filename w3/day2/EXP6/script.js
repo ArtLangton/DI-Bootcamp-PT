8ac2c4f783e78a250e5fc2cc5396a95e2333abed
ArtLangton <artemlangton@gmail.com>
2023-12-17 13:03:48 +0300

    changes to EXP6 in day2

--- w3/day2/EXP6/script.js
+++ w3/day2/EXP6/script.js
@@ -1,55 +1,55 @@
 5 + "34"
-// Prediction:
-// Actual:
+// Prediction: 534 (34 is text)
+// Actual: 534 
 
 5 - "4"
-// Prediction:
-// Actual:
+// Prediction: NaN
+// Actual: 1
 
 10 % 5
-// Prediction:
-// Actual:
+// Prediction: 0
+// Actual: 0
 
 5 % 10
-// Prediction:
-// Actual:
+// Prediction: 5
+// Actual: 5
 
 "Java" + "Script"
-// Prediction:
-// Actual:
+// Prediction: JavaScript
+// Actual: JavaScript
 
 " " + " "
 // Prediction:
-// Actual:
+// Actual: '  '
 
 " " + 0
-// Prediction:
-// Actual:
+// Prediction:  0
+// Actual: ' 0'
 
 true + true
-// Prediction:
-// Actual:
+// Prediction: 2
+// Actual: 2
 
 true + false
-// Prediction:
-// Actual:
+// Prediction: 1
+// Actual: 1
 
 false + true
-// Prediction:
-// Actual:
+// Prediction: 1
+// Actual: 1
 
 false - true
-// Prediction:
-// Actual:
+// Prediction: -1
+// Actual: -1
 
 !true
-// Prediction:
-// Actual:
+// Prediction: false
+// Actual: false
 
 3 - 4
-// Prediction:
-// Actual:
+// Prediction: -1
+// Actual: -1
 
 "Bob" - "bill"
-// Prediction:
-// Actual:
+// Prediction: NaN
+// Actual: NaN
