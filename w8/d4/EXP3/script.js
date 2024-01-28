f5308442cdd3346bc385cf1cfb72b92c346b45b7
ArtLangton <artemlangton@gmail.com>
2024-01-28 15:27:54 +0300

    finished exp 3 (day 4) week 8

--- w8/d4/EXP3/script.js
+++ w8/d4/EXP3/script.js
@@ -1,21 +1,7 @@
-function formElement() {
-    document.getElementById('fortuneForm').addEventListener('submit', function (e) {
-        e.preventDefault();
+const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
 
-        const jobValue = document.getElementById('job').value;
-        const locationValue = document.getElementById('location').value;
-        const partnerValue = document.getElementById('partner').value;
-        const childrenValue = document.getElementById('children').value;
+const combinedString = epic.reduce((accumulator, current) => {
+  return accumulator + ' ' + current;
+});
 
-        fortune(jobValue, locationValue, partnerValue, childrenValue);
-    });
-}
-
-
-};
-
-formElement();
-
-})()
-
-// I need some time to finish it, sorry. I will upload ASAP
+console.log(combinedString.trim());
